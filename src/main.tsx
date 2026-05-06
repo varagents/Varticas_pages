fb03ac0250002a4a373bc36a41191ba470c63dc0
Raj Barnawal <rjking0568@gmail.com>
2026-05-06 22:21:22 +0530

    analytics added again

--- src/main.tsx
+++ src/main.tsx
@@ -2,32 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-declare global {
-  interface Window {
-    dataLayer: unknown[];
-    gtag: (...args: unknown[]) => void;
-  }
-}
-
-function initGoogleAnalytics() {
-  const gaId = import.meta.env.VITE_GA_ID as string | undefined;
-  if (!import.meta.env.PROD || !gaId) {
-    return;
-  }
-
-  const script = document.createElement("script");
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-  document.head.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = (...args: unknown[]) => {
-    window.dataLayer.push(args);
-  };
-  window.gtag("js", new Date());
-  window.gtag("config", gaId);
-}
-
-initGoogleAnalytics();
-
 createRoot(document.getElementById("root")!).render(<App />);
